4b9b56f4b79b4b582ec701a867a3dc8884e95c65
Yusuke Naito <h7110y@gmail.com>
2016-03-14 19:45:51 +0900

    Introduce Area model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160314055114) do
+ActiveRecord::Schema.define(version: 20160314093426) do
+
+  create_table "areas", force: :cascade do |t|
+    t.string   "name"
+    t.text     "coordinates"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "spots", force: :cascade do |t|
     t.string   "name"
