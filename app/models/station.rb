d0bea99612e9e9af66bb7896ac651acbc5b0fc38
Yusuke Naito <h7110y@gmail.com>
2016-03-14 18:23:11 +0900

    Add index settings to Station

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,2 +1,16 @@
 class Station < ActiveRecord::Base
+  include Concerns::Searchable
+
+  mapping do
+    indexes :id, type: 'string', index: 'not_analyzed'
+    indexes :name, type: 'string', analyzer: 'kuromoji'
+    indexes :location, type: 'geo_point'
+  end
+
+  def as_indexed_json(options = {})
+    { 'id'       => id,
+      'name'     => name,
+      'location' => "#{lat},#{lon}",
+    }
+  end
 end
