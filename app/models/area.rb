0f77bfc5585b8688318ad4d0c78c6ca98cad0e6c
Yusuke Naito <h7110y@gmail.com>
2016-03-14 19:46:33 +0900

    Add search index settings to Area

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,3 +1,21 @@
 class Area < ActiveRecord::Base
+  include Concerns::Searchable
+
+  mapping do
+    indexes :id, type: 'string', index: 'not_analyzed'
+    indexes :name, type: 'string', analyzer: 'kuromoji'
+    indexes :location, type: 'geo_shape'
+  end
+
+  def as_indexed_json(options = {})
+    { 'id'       => id,
+      'name'     => name,
+      'location' => {
+        'type' => 'polygon',
+        'coordinates' => [coordinates],
+      }
+    }
+  end
+
   serialize :coordinates
 end
