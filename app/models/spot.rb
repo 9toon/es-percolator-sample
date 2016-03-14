0238ff8c16ac1c0b47247e4023339667cbe63d6a
Yusuke Naito <h7110y@gmail.com>
2016-03-14 14:36:38 +0900

    Add elasticsearch settings to Spot

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,2 +1,21 @@
 class Spot < ActiveRecord::Base
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
+  index_name "#{Rails.env}-#{Rails.application.class.to_s.downcase}-#{self.name.downcase}"
+
+  mapping do
+    indexes :id, type: 'string', index: 'not_analyzed'
+    indexes :name, type: 'string', analyzer: 'kuromoji'
+    indexes :address, type: 'string', analyzer: 'kuromoji'
+    indexes :location, type: 'geo_point'
+  end
+
+  def as_indexed_json(options = {})
+    { 'id'       => id,
+      'name'     => name,
+      'address'  => address,
+      'location' => "#{lat},#{lon}",
+    }
+  end
 end
