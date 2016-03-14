d52450a99f60f49d41ced45e7d8358830476c2c2
Yusuke Naito <h7110y@gmail.com>
2016-03-14 18:29:08 +0900

    Add Station.create_distance_percolates

--- app/models/station.rb
+++ app/models/station.rb
@@ -13,4 +13,30 @@ class Station < ActiveRecord::Base
       'location' => "#{lat},#{lon}",
     }
   end
+
+  def self.create_distance_percolates(radius: 1000)
+    Station.all.each do |station|
+      args = {
+        index: self.__elasticsearch__.index_name,
+        type: '.percolator',
+        id: "station-distance-#{station.id}-#{radius}",
+        body: {
+          query: {
+            filtered: {
+              filter: {
+                geo_distance: {
+                  location: {
+                    lat: station.lat,
+                    lon: station.lon,
+                  },
+                  distance: "#{radius}meters",
+                }
+              }
+            }
+          }
+        }
+      }
+      self.__elasticsearch__.client.index(args)
+    end
+  end
 end
